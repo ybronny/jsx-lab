960f3cc2a4b177c729f4f5ad1e4218b2c168a79a
ronny <ronnymwangi7@gmail.com>
2024-04-30 09:40:01 +0300

    Assignment completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Regera is the best looking Koenigsegg</p>
+    <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"></img>
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const nwame = "Ronny";
+const city = "Nairobi";
+
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1>Ronny is a Web Developer from Nairobi</h1>
+  </div>;
 }
 
 export default Home;
